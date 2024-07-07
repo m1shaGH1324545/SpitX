72a2618a3ba6b7294a73117c768a55590398657b
1Krokko3213 <99880988+m1shaGH1324545@users.noreply.github.com>
2024-07-07 21:30:13 +0800

    Add files via upload

---
+++ src/main/java/org/example/pl/spitx/SpitX.kt
@@ -0,0 +1,22 @@
+package org.example.pl.spitx
+
+import org.bukkit.Bukkit
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+
+class SpitX : JavaPlugin() {
+    override fun onEnable() {
+
+        instance = this
+        saveDefaultConfig()
+        getCommand("spit")!!.setExecutor(SpitCMD(config as YamlConfiguration))
+        getCommand("spit")!!.tabCompleter = SpitCMD(config as YamlConfiguration)
+        Bukkit.getLogger().fine("SpitX был успешно запущен")
+
+    }
+    companion object {
+        @JvmStatic
+        var instance: SpitX? = null
+            private set
+    }
+}
